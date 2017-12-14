ac95bfafa4da0f9e06a38efb420e1ead440d2485
Mathee Sivananthan <s.mathee_92@live.ca>
2017-12-13 20:56:17 -0500

    First commit

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use((req, res, next) => {
   var now = new Date().toString();
   var log = `${now}: ${req.method} ${req.url}`;
 
+
   console.log(log);
   fs.appendFile('server.log', log + '\n');
   next();
